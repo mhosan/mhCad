17f994998f8824b6f89d8ad30f303d7e6af293af
mhosan <mhosan@gmail.com>
2023-11-09 10:32:04 -0300

    primer commit

---
+++ modDeclaraciones.vb
@@ -0,0 +1,37 @@
+﻿Imports System.IO
+
+Module modDeclaraciones
+    Public ultimoComando As String
+    Public ubicacionSoporte As String
+    Public ubicacionHelp As String
+    Public aplicacionNombre As String
+    Public sepDecimal As String
+    Public sepMiles As String
+    Public entidad As VectorDraw.Professional.vdPrimaries.vdFigure
+    Public seguir As Boolean = True
+    Public distanciaOffset As Double
+    Public valorOsnap As String
+    Public poligonosCantidad As Integer
+    Public ufCantidad As Integer
+    Public ucCantidad As Integer
+    Public scCantidad As Integer
+    'Public matrizEspesores As VectorDraw.Geometry.DoubleArray = New VectorDraw.Geometry.DoubleArray(255)
+    Public matrizEspesores(255) As Double
+    Public versionVisadores As Boolean
+
+    Sub New()
+        '--------------
+        ' el help
+        '--------------
+        ubicacionHelp = Application.StartupPath
+        frmPpal.HelpProvider1.HelpNamespace = ubicacionHelp & "\mhcad.chm"
+        'Dim soporteDefault As Object
+        'soporteDefault = frmPpal.VdF.BaseControl.ActiveDocument.SupportPath
+        '--------------
+        ' soporte
+        '--------------
+        ubicacionSoporte = Application.StartupPath & "\mhcadt\"
+
+    End Sub
+
+End Module
---
+++ modGenerarHash.vb
@@ -0,0 +1,31 @@
+﻿Module modGenerarHash
+
+    Public Function genero_hash(ByVal pathArch As String) As String
+        '------------------------------------------------------------
+        'calcular el hash
+        'pathArch es el nombre del archivo original con path incluido
+        '------------------------------------------------------------
+        Dim Hash As CAPICOM.HashedData
+        Dim NroF As Integer
+        Dim StrData As String
+
+        Hash = New CAPICOM.HashedData
+        Hash.Algorithm = CAPICOM.CAPICOM_HASH_ALGORITHM.CAPICOM_HASH_ALGORITHM_MD5
+        NroF = FreeFile()
+
+        FileOpen(1, pathArch, OpenMode.Binary)
+        StrData = InputString(1, FileLen(pathArch)) 'revisar aqui!!!, debe ser una lectura binaria...ojo
+        'Dim leidoB() As Byte = File.ReadAllBytes(StrData)
+
+        FileClose(1)
+
+        Hash.Hash(StrData)
+        'Hash.Hash(leidoB.ToString)
+        genero_hash = Hash.Value
+
+
+        'Dim oUtilities As CAPICOM.Utilities
+        'oUtilities.BinaryStringToByteArray()
+
+    End Function
+End Module
